8119a661474795231e40d07a99666ff69ef599a8
Felipe Alves <134858772+Felipe-Alvess@users.noreply.github.com>
2023-11-06 20:29:30 -0300

    Minha calculadora

---
+++ script.js
@@ -0,0 +1,18 @@
+let displayValue = "";
+
+function appendTodisplay(value){
+displayValue += value;
+document.getElementById("display").value = displayValue;
+}
+function clearDisplay (){
+    displayValue = "";
+    document.getElementById("display").value = displayValue;
+}
+function calculate(){
+    try{
+        displayValue = eval(displayValue);
+        document.getElementById("display").value = displayValue;
+    }catch (error){
+        document.getElementById("displayValue").value = "Error"
+    }
+}
